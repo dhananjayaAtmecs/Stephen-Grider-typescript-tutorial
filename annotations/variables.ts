31b3847e14b608aac5e9c33fae246654ce42f4f2
dhananjaya <dhananjaya.kr@atmecs.com>
2022-06-09 00:12:19 +0530

    09-06-2022

---
+++ annotations/variables.ts
@@ -0,0 +1,11 @@
+let apple: number = 7;
+let speed: string = 'fast';
+let hasName: boolean = true;
+
+let nothingMuch: null = null;
+let nothing: undefined = undefined;
+
+apple = 10;
+
+// built in objects
+let now: Date = new Date();
